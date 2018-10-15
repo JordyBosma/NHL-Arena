6123177a178a2cc6e517065c51ebb88f6dbda928
JorunnO <oost1704@student.nhl.nl>
2018-10-15 14:51:57 +0200

    Added first person camera movement

--- NhlArena VS/wwwroot/js/BasicPlane.js
+++ NhlArena VS/wwwroot/js/BasicPlane.js
@@ -5,8 +5,8 @@
     }
     init() {
         var selfRef = this;
-        var geometry = new THREE.BoxGeometry(20, 2, 20);
-        var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+        var geometry = new THREE.BoxGeometry(200, 2, 200);
+        var material = new THREE.MeshBasicMaterial({ color: 0xffffff});
 
         var box = new THREE.Mesh(geometry, material);
         box.position.x = -10;
