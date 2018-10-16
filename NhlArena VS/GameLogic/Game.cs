c79c47ccdc5bf1d88e77f4d7c3cd79ab8c7e38fb
Pugnar <32259799+SwenvdWijngaard@users.noreply.github.com>
2018-10-16 15:52:06 +0200

    sockets creation + game structure

---
+++ NhlArena VS/GameLogic/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using Views;
+
+namespace NhlArena_VS.GameLogic
+{
+    public class Game
+    {
+        private bool isActive = false;
+        public Guid gameId { get; }
+        List<Client> players = new List<Client>();
+
+        public Game(Client initailPlayer)
+        {
+            gameId = new Guid();
+            players.Add(initailPlayer);
+
+            Thread gameThread = new Thread(Logic);
+            gameThread.Start();
+        }
+
+        public void AddPlayer(Client newPlayer)
+        {
+            if(players.Count() != 6)
+            {
+                players.Add(newPlayer);
+            }
+            else
+            {
+                //playermax reached
+            }
+        }
+
+        public void Logic()
+        {
+            isActive = true;
+
+            while (isActive)
+            {
+                Thread.Sleep(25);               
+            }
+        }
+    }
+}
---
+++ NhlArena VS/GameLogic/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Views;
+
+namespace NhlArena_VS.GameLogic
+{
+    public static class GameManager
+    {
+        static List<Game> activeGames = new List<Game>();
+        
+        public static void ManageClient(Client cs)
+        {
+            
+        }
+    }
+}
