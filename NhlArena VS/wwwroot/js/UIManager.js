ba1730dc38fc5e6cf89f25781009d8ebc8f87b8d
mastermaarten <mastermaarten99@gmail.com>
2018-10-19 15:31:48 +0200

    beetje testen met effects

---
+++ NhlArena VS/wwwroot/js/UIManager.js
@@ -0,0 +1,21 @@
+﻿class UIManager {
+    constructor(x) {
+        this.menu = document.getElementById("MainMenu");
+    }
+
+    ShowMenu(b) {
+        //this.Show(this.menu, b);
+        document.getElementById("toggle-1").checked = b;
+    }
+
+    Show(x, b) {
+        if (b) {
+            x.classList.remove("hiddenEffect");
+            x.classList.add("showEffect");
+        } else {
+            x.classList.remove("showEffect");
+            x.classList.add("hiddenEffect");
+        }
+        
+    }
+}
