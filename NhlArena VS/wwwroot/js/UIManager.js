f5c9f1360789b4cb8cbc59cee74ae80da4145667
mastermaarten <mastermaarten99@gmail.com>
2018-10-29 15:46:16 +0100

    UI timer fix and part fix and kd count

--- NhlArena VS/wwwroot/js/UIManager.js
+++ NhlArena VS/wwwroot/js/UIManager.js
@@ -2,9 +2,24 @@
     constructor(x) {
         this.volume = true;
         this.openMenu = "";
+
         this.timeLeft = 0;  //in seconds
+        this.isRunning = false;
+        this.runTimer;
         this.StartTimer(360);
-        this.timer = null;
+        this.UpdateGameScores(10, 10);
+    }
+
+    UpdateGameScores(K, D) {
+        this.UpdateGameScore(K, "gameScoreK", "K: ");
+        this.UpdateGameScore(D, "gameScoreD", "D: ");
+    }
+    UpdateGameScore(Val, Type, Prompt) {
+        Val = String(Val);
+        if (Val.length < 2) {
+            Val = "0" + Val;
+        }
+        document.getElementById(Type).innerText = Prompt + Val;
     }
 
     ShowOptionMenu(b) {
@@ -14,7 +29,7 @@
     ShowExitMenu(b) {
         document.getElementById("toggle-ExitMenu").checked = b;
     }
-    
+
     SwitchSoundOnOff(clicked) {
         var newInnerText;
         if (clicked.innerText === "volume_up") {
@@ -31,31 +46,44 @@
     }
 
     StartTimer(time) {
+        this.StopTimer();
         this.timeLeft = time;
-        this.timer = setInterval(this.UpdateTimer(), 1000);
+        var cls = this;
+        this.runTimer = setInterval(function () {
+            cls.UpdateTimer(cls);
+        }, 1000);
+        this.isRunning = true;
+        console.log("start");
     }
 
-    UpdateTimer() {
-        this.timeLeft--;
-        console.log(this.timeLeft);
-        var displayTimeMin = Math.floor(this.timeLeft / 60);
+    UpdateTimer(cls) {
+        cls.timeLeft = cls.timeLeft - 1;
+        var displayTimeMin = Math.floor(cls.timeLeft / 60);
         displayTimeMin = displayTimeMin.toString();
         if (displayTimeMin.length < 2) {
             displayTimeMin = '0' + displayTimeMin;
         }
-        var displayTimeSec = String(this.timeLeft % 60);
+        var displayTimeSec = String(cls.timeLeft % 60);
         if (displayTimeSec.length < 2) {
             displayTimeSec = '0' + displayTimeSec;
         }
         var displayTime = displayTimeMin + ':' + displayTimeSec;
         document.getElementById('gameTimer').innerText = displayTime;
-        if (this.timeLeft === 0) {
-            //setTimeout(this.UpdateTimer(), 1000000);
-            clearInterval(this.Timer);
+        if (cls.timeLeft <= 0) {
+            
+            cls.StopTimer();
+        }
+    }
+
+    StopTimer() {
+        if (this.isRunning) {
+            this.isRunning = false;
+            clearInterval(this.runTimer);
             console.log("stop");
         }
     }
 
+    /*
     Show(x, b) {
         if (b) {
             x.classList.remove("hiddenEffect");
@@ -65,5 +93,5 @@
             x.classList.add("hiddenEffect");
         }
         
-    }
+    }*/
 }
