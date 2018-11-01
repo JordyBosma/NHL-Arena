f55340710906d0f49f8d0e43dd56e9dcdb1a61e7
mastermaarten <mastermaarten99@gmail.com>
2018-11-01 11:19:28 +0100

    ordering added, ready for testing

--- NhlArena VS/wwwroot/js/UIManager.js
+++ NhlArena VS/wwwroot/js/UIManager.js
@@ -82,8 +82,41 @@
         }
     }
 
-    OrderScoreboardScore() {
-
+    OrderScoreboardScore() {            //https://www.w3schools.com/howto/howto_js_sort_table.asp
+        var rows, switching, i, x, y, shouldSwitch;
+        switching = true;
+        /*Make a loop that will continue until
+        no switching has been done:*/
+        while (switching) {
+            //start by saying: no switching is done:
+            switching = false;
+            rows = this.scores.rows;
+            /*Loop through all table rows (except the
+            first, which contains table headers):*/
+            for (i = 1; i < (rows.length - 1); i++) {
+                //start by saying there should be no switching:
+                shouldSwitch = false;
+                /*Get the two elements you want to compare,
+                one from current row and one from the next:*/
+                x = rows[i].getElementsByTagName("TD")[2];
+                y = rows[i + 1].getElementsByTagName("TD")[2];
+                //check if the two rows should switch place:
+                if (Number(x.innerHTML) > Number(y.innerHTML)) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+            if (shouldSwitch) {
+                /*If a switch has been marked, make the switch
+                and mark that a switch has been done:*/
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+            }
+        }
+        for (var j = 0; j < rows.length; j++) {
+            rows[j].getElementsByTagName("TD")[0].innerHTML = string(j+1) + ".";
+        }
     }
 
     RemoveScoreboardScore(guid) {
